dacf447879757e4796e0cb569a943e3110b71e52
Kaikei-e <e_kaikei@protonmail.com>
2022-12-28 23:59:52 +0900

    Update: configured the embed functionality

--- main.go
+++ main.go
@@ -10,10 +10,20 @@ import (
 //go:embed build/*
 //go:embed build/index.html
 //go:embed build/_app/*
+//go:embed build/_app/*/*.js
+//go:embed build/_app/immutable/chunks/*.js 
+//go:embed build/_app/immutable/components/pages/*.js 
+//go:embed static/output.css
 
 var publicFS embed.FS
 
 func main() {
+	static, err := fs.Sub(publicFS, "static")
+	if err != nil {
+		panic(err)
+	}
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(static))))
+
 	public, err := fs.Sub(publicFS, "build")
 	if err != nil {
 		panic(err)
